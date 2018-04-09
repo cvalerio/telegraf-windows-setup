1fe0a8a8696a54208baf0b3b02e70751baa8ca5b
Claudio Valerio <claudio.valerio@zero.it>
2018-04-09 12:29:41 +0200

    Initialize scripts

---
+++ setup.ps1
@@ -0,0 +1,37 @@
+if(!(Get-Command choco -errorAction SilentlyContinue)){
+	Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+choco install -y telegraf
+
+function Read-Host-With-Default($prompt, $default) {
+	if(($result = Read-Host "$prompt [$default]") -eq ''){
+	return $default
+	}else{
+	return $result
+	}
+}
+
+$conftemplate = '.\telegraf.conf.template'
+$confdestination = 'c:\Program Files\telegraf\telegraf.conf'
+
+$dbhost = Read-Host-With-Default 'InfluxDB Server' 'http://localhost:8089'
+$dbname = Read-Host-With-Default 'DB name' 'telegraf'
+$dbusername = Read-Host-With-Default 'DB user name' ''
+$dbpassword = Read-Host-With-Default 'DB pass word' ''
+$hostname = Hostname
+$hostname = Read-Host-With-Default 'This host name' $hostname
+
+copy $confdestination "$confdestination.copy"
+
+(Get-Content $conftemplate) | Foreach-Object {
+    $_ -replace '{{DB_HOST}}', $dbhost `
+       -replace '{{DB_NAME}}', $dbname `
+       -replace '{{DB_USER_NAME}}', $dbusername `
+       -replace '{{DB_USER_NAME}}', $dbpassword `
+       -replace '{{HOSTNAME}}', $hostname
+    } | Set-Content $confdestination
+	
+cd "c:\Program Files\telegraf\"
+telegraf.exe --service install
+net start telegraf
